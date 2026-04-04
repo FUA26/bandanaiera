9569c850c3fb6c41a5f7393a909168854e62e000
fatihua <fatihua@gmail.com>
2026-04-04 10:41:40 +0700

    feat: add FieldContent, FieldError, FieldLabel components

--- packages/feedback-ui/src/ui/field.tsx
+++ packages/feedback-ui/src/ui/field.tsx
@@ -1,4 +1,6 @@
 import * as React from "react"
+import { cn } from "@workspace/utils"
+import { Label } from "./label"
 
 const Field = React.forwardRef<
   HTMLDivElement,
@@ -8,4 +10,40 @@ const Field = React.forwardRef<
 ))
 Field.displayName = "Field"
 
-export { Field }
+const FieldContent = React.forwardRef<
+  HTMLDivElement,
+  React.ComponentPropsWithoutRef<"div">
+>(({ className, ...props }, ref) => (
+  <div ref={ref} className={cn("space-y-2", className)} {...props} />
+))
+FieldContent.displayName = "FieldContent"
+
+const FieldError = React.forwardRef<
+  HTMLParagraphElement,
+  React.ComponentPropsWithoutRef<"p">
+>(({ className, children, ...props }, ref) => {
+  return (
+    <p
+      ref={ref}
+      className={cn("text-sm font-medium text-destructive", className)}
+      {...props}
+    >
+      {children}
+    </p>
+  )
+})
+FieldError.displayName = "FieldError"
+
+const FieldLabel = React.forwardRef<
+  HTMLParagraphElement,
+  React.ComponentPropsWithoutRef<"p"> & React.LabelHTMLAttributes<HTMLLabelElement>
+>(({ className, children, ...props }, ref) => {
+  return (
+    <Label ref={ref} className={cn("font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70", className)} {...props}>
+      {children}
+    </Label>
+  )
+})
+FieldLabel.displayName = "FieldLabel"
+
+export { Field, FieldContent, FieldError, FieldLabel }
